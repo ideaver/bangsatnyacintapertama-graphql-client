ece9f9f159dee7a1fed3b3ede9d7b438121097d7
Elriz Wiraswara <elrizwiraswara@gmail.com>
2023-09-15 10:00:05 +0700

    first commit

---
+++ example/bangsatnyacintapertama_graphql_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:bangsatnyacintapertama_graphql_client/bangsatnyacintapertama_graphql_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/bangsatnyacintapertama_graphql_client_test.dart
@@ -0,0 +1,16 @@
+import 'package:bangsatnyacintapertama_graphql_client/bangsatnyacintapertama_graphql_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
